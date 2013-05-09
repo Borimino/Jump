6dcd94c76221bd8e398c1b33fd2401cd80d57f57
Borimino <MarkusAndreassen1994@gmail.com>
2013-05-09 18:07:09 +0200

    Tried to add an automatic level reader
    Danger-detection not working yet

--- field.py
+++ field.py
@@ -6,11 +6,15 @@ from math import e, pi, cos, sin, sqrt
 from random import uniform
 
 class Field:
-	def __init__(self, x, y):
+	def __init__(self, pos=(-1,-1), x=0, y=0):
 		self.x = x
 		self.y = y
+		if pos[0] != -1 and pos[1] != -1:
+			self.x = pos[0]
+			self.y = pos[1]
 
 	danger = 0
+	available = 0
 
 	def get_x(self):
 		return self.x	
@@ -21,9 +25,51 @@ class Field:
 	def get_danger(self):
 		return self.danger
 
+	def is_danger(self, main):
+		for field in main.fields:
+			if (field.x - self.x) == (field.y - self.y):
+				if field.danger == 2:
+					self.danger == 1
+			if self.x - field.x == field.y - self.y:
+				if field.danger == 2:
+					self.danger == 1
+			if field.x - self.x == self.y - field.y:
+				if field.danger == 2:
+					self.danger == 1
+			if self.x - field.x == self.y - field.y:
+				if field.danger == 2:
+					self.danger == 1
+
 	def draw(self, main):
 		if self.danger == 0:
 			pygame.draw.rect(main.screen, main.color_field, (self.x*main.fieldsize, self.y*main.fieldsize, main.fieldsize, main.fieldsize))
-		else:
+			if self.is_available(main) == 1:
+				pygame.draw.rect(main.screen, main.color_available, (self.x*main.fieldsize, self.y*main.fieldsize, main.fieldsize, main.fieldsize))
+				pygame.draw.rect(main.screen, main.color_outline, (self.x*main.fieldsize, self.y*main.fieldsize, main.fieldsize, main.fieldsize),1)
+		elif self.danger == 1:
 			pygame.draw.rect(main.screen, main.color_danger, (self.x*main.fieldsize, self.y*main.fieldsize, main.fieldsize, main.fieldsize))
-		pygame.draw.rect(main.screen, main.color_outline, (self.x*main.fieldsize, self.y*main.fieldsize, main.fieldsize, main.fieldsize),1)
+			pygame.draw.rect(main.screen, main.color_outline, (self.x*main.fieldsize, self.y*main.fieldsize, main.fieldsize, main.fieldsize),1)
+		elif self.danger == 2:
+			pass
+		else:
+			pass
+	
+	def is_available(self, main):
+		if self.danger == 1:
+			return 0
+		if main.player[0] + main.path[0] == self.x and main.player[1] + main.path[1] == self.y:
+			return 1
+		if main.player[0] - main.path[0] == self.x and main.player[1] + main.path[1] == self.y:
+			return 1
+		if main.player[0] + main.path[0] == self.x and main.player[1] - main.path[1] == self.y:
+			return 1
+		if main.player[0] - main.path[0] == self.x and main.player[1] - main.path[1] == self.y:
+			return 1
+		if main.player[0] + main.path[1] == self.x and main.player[1] + main.path[0] == self.y:
+			return 1
+		if main.player[0] - main.path[1] == self.x and main.player[1] + main.path[0] == self.y:
+			return 1
+		if main.player[0] + main.path[1] == self.x and main.player[1] - main.path[0] == self.y:
+			return 1
+		if main.player[0] - main.path[1] == self.x and main.player[1] - main.path[0] == self.y:
+			return 1
--- jump.py
+++ jump.py
@@ -10,18 +10,33 @@ class Starter(PygameHelper):
 	def __init__(self):
 		self.w, self.h = 800, 600
 		PygameHelper.__init__(self, size=(self.w, self.h), fill=((0,0,0)))
+		f = open("coords.txt")
+		lines = f.readlines()
+		for line in lines:
+			coords = line.split()
+			f2 = Field(x=int(coords[0]), y=int(coords[1]))
+			if int(coords[2]) == 2:
+				f2.danger = 2
+			#f2.danger = int(coords[2])
+			self.fields.append(f2)
+		for field in self.fields:
+			field.is_danger(self)
+		for field in self.fields:
+			print("x = ", field.x, " & y = ", field.y, " & d = ", field.danger)
 
 	fieldsize = 100
 	fields = []
-	for i in range(5):
-		for j in range(5):
-			f = Field(i, j)
-			fields.append(f)
+#	for i in range(5):
+#		for j in range(5):
+#			f = Field(x=i, y=j)
+#			fields.append(f)
 	color_field = (255,255,255)
 	color_outline = (255,0,0)
-	color_player = (0,255,0)
+	color_player = (255,0,0)
 	color_danger = (0,0,255)
+	color_available = (0,255,0)
 	player = (0,0)
+	path = (1,2)
 
 	def update(self):
 		pass
@@ -33,12 +48,10 @@ class Starter(PygameHelper):
 		pass
 
 	def mouseDown(self, button, pos):
-		print('button down at: ', pos)
 		for field in self.fields:
-			print('testing field at: ', field.get_x(), field.get_y())
 			if pos[0] > field.get_x()*self.fieldsize and pos[0] < field.get_x()*self.fieldsize+self.fieldsize and pos[1] > field.get_y()*self.fieldsize and pos[1] < field.get_y()*self.fieldsize+self.fieldsize:
-				self.player = (field.get_x(), field.get_y())
-		print('player at: ', self.player)
+				if field.is_available(self) == 1:
+					self.player = (field.get_x(), field.get_y())
 
 
 	def mouseUp(self, button, pos):
